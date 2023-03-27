25232beb39a2445389cc778425fe601ec8820bd4
AnuragJha003 <97457228+AnuragJha003@users.noreply.github.com>
2023-03-27 10:54:47 +0530

    Create UniqueTreeConsturctReq.cpp

---
+++ UniqueTreeConsturctReq.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    bool isPossible(int a,int b)
+    {
+        //code here
+         if( (a==2 || b==2) && (a!=b) )
+            return true;
+        else
+            return false;
+    }
+};//
+// we can create unique binary tree with any order(preorder or postorder) + inorder
+
+https://practice.geeksforgeeks.org/problems/unique-binary-tree-requirements/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=unique-binary-tree-requirements
